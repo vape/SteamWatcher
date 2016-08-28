ccf1b2fbc7bfc577b66e26042fdcb8624334f738
Artem Sokolov <sokolov.artem@hotmail.com>
2016-08-28 13:32:08 +1000

    Added pricechange data object.

---
+++ src/SteamWatcher/Data/PriceChange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamWatcher.Data
+{
+    public struct PriceChange
+    {
+        public readonly int AppID;
+        public readonly PriceInfo Previous;
+        public readonly PriceInfo New;
+        public readonly DateTime Updated;
+
+        public PriceChange(int appId, PriceInfo previous, PriceInfo newInfo)
+        {
+            AppID = appId;
+            Previous = previous;
+            New = newInfo;
+            Updated = DateTime.UtcNow;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{Previous.ToString()}}} -> {{{New.ToString()}}}";
+        }
+    }
+}
